550677bb15d82412707f1e4bb47d2ee5efecdfda
Nolan Locke <nolan.s.locke@gmail.com>
2024-01-03 00:58:56 +0000

    comment keybinds

--- home/hyprland/keybinds.nix
+++ home/hyprland/keybinds.nix
@@ -5,10 +5,18 @@
 }: {
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
+
     bind = [
+      # { Mod + Enter } = open terminal
       "$mod, RETURN, exec, kitty"
+
+      # { Mod + Shift + C } = close active window
       "$mod SHIFT, c, killactive"
+
+      # { Mod + Space } = open app launcher
       "$mod, SPACE, exec, rofi -show drun"
+
+      # { Mod + [1..9] } = open workspace
       "$mod, 1, workspace, 1"
       "$mod, 2, workspace, 2"
       "$mod, 3, workspace, 3"
@@ -18,7 +26,8 @@
       "$mod, 7, workspace, 7"
       "$mod, 8, workspace, 8"
       "$mod, 9, workspace, 9"
-      "$mod, 0, workspace, 10"
+
+      # { Mod + Shift + [1..9] } = move window to workspace
       "$mod SHIFT, 1, movetoworkspace, 1"
       "$mod SHIFT, 2, movetoworkspace, 2"
       "$mod SHIFT, 3, movetoworkspace, 3"
@@ -29,13 +38,17 @@
       "$mod SHIFT, 8, movetoworkspace, 8"
       "$mod SHIFT, 9, movetoworkspace, 9"
       "$mod SHIFT, 0, movetoworkspace, 10"
+
+      # { Mod + S } = Special workspace for spotify
       "$mod, S, togglespecialworkspace, spotify"
       "$mod SHIFT, S, movetoworkspace, special:spotify"
     ];
 
     bindm = [
-      # Move/resize windows with mainMod + LMB/RMB and dragging
+      # { Mod + LMB } = Move window
       "$mod, mouse:272, movewindow"
+
+      # { Mod + RMB } = Resize window
       "$mod, mouse:273, resizewindow"
     ];
   };
