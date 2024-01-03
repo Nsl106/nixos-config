366c354fb3da1094d24eb8e54e6092f73d977679
Nolan Locke <nolan.s.locke@gmail.com>
2024-01-03 05:07:36 +0000

    don't follow window into workspaces

--- home/hyprland/keybinds.nix
+++ home/hyprland/keybinds.nix
@@ -39,20 +39,20 @@
       "$mod, 0, workspace, 10"
 
       # { Mod + Shift + [0..9] } = move window to workspace
-      "$mod SHIFT, 1, movetoworkspace, 1"
-      "$mod SHIFT, 2, movetoworkspace, 2"
-      "$mod SHIFT, 3, movetoworkspace, 3"
-      "$mod SHIFT, 4, movetoworkspace, 4"
-      "$mod SHIFT, 5, movetoworkspace, 5"
-      "$mod SHIFT, 6, movetoworkspace, 6"
-      "$mod SHIFT, 7, movetoworkspace, 7"
-      "$mod SHIFT, 8, movetoworkspace, 8"
-      "$mod SHIFT, 9, movetoworkspace, 9"
-      "$mod SHIFT, 0, movetoworkspace, 10"
+      "$mod SHIFT, 1, movetoworkspacesilent, 1"
+      "$mod SHIFT, 2, movetoworkspacesilent, 2"
+      "$mod SHIFT, 3, movetoworkspacesilent, 3"
+      "$mod SHIFT, 4, movetoworkspacesilent, 4"
+      "$mod SHIFT, 5, movetoworkspacesilent, 5"
+      "$mod SHIFT, 6, movetoworkspacesilent, 6"
+      "$mod SHIFT, 7, movetoworkspacesilent, 7"
+      "$mod SHIFT, 8, movetoworkspacesilent, 8"
+      "$mod SHIFT, 9, movetoworkspacesilent, 9"
+      "$mod SHIFT, 0, movetoworkspacesilent, 10"
 
       # { Mod + S } = Special workspace for spotify
       "$mod, S, togglespecialworkspace, spotify"
-      "$mod SHIFT, S, movetoworkspace, special:spotify"
+      "$mod SHIFT, S, movetoworkspacesilent, special:spotify"
     ];
 
     bindm = [
