c8327695606b5e19d33ce863b46bcf5dfc5d29c9
Nolan Locke <nolan.s.locke@gmail.com>
2024-01-03 11:02:13 -0700

    add second monitor

--- home/hyprland/monitors.nix
+++ home/hyprland/monitors.nix
@@ -1,7 +1,9 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-3,2560x1440@165,0x0,1"
+      # NAME,RESOLUTION@REFRESHRATE,POSITON,SCALE
+      "DP-3,2560x1440@165,2560x0,1"
+      "DP-2,2560x1440@165,0x0,1"
     ];
   };
 }
