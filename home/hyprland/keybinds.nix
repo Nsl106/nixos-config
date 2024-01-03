349acd8a12176a9df286381e7af5a1ade21b67ed
Nolan Locke <nolan.s.locke@gmail.com>
2024-01-03 05:02:14 +0000

    more audio keys

--- home/hyprland/keybinds.nix
+++ home/hyprland/keybinds.nix
@@ -9,6 +9,14 @@
       ", XF86AudioMute, exec, pamixer --toggle-mute"
       ", XF86AudioPlay, exec, playerctl play-pause"
 
+      # { Mod + Shift + Left/Right Arrow } = next/previous song
+      "$mod SHIFT, Right, exec, playerctl next"
+      "$mod SHIFT, Left, exec, playerctl previous"
+
+      # { Mod + Left/Right Arrow } = skip/go back 5 seconds
+      "$mod, Right, exec, playerctl position 5+"
+      "$mod, Left, exec, playerctl position 5-"
+
       # { Mod + Enter } = open terminal
       "$mod, RETURN, exec, kitty"
 
