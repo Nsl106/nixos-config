57a57600095698361535a1575eea3471ba451b92
Nolan Locke <nolan.s.locke@gmail.com>
2024-01-05 21:14:24 -0700

    add manual grub entry for windows

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -19,6 +19,14 @@
     enable = true;
     # set $FS_UUID to the UUID of the EFI partition
     useOSProber = true;
+    extraEntries = ''
+      menuentry "Windows" {
+          	insmod chain
+          	insmod ntfs
+          	set root=(hd1,1)
+          	chainloader +1
+      }
+    '';
   };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
