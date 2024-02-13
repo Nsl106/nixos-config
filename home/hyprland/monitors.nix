327c6082615d598b9a4d4ad5868d79945640e747
Nolan Locke <nolan.s.locke@gmail.com>
2024-02-13 11:23:00 -0700

    fix monitor refresh rate

--- home/hyprland/monitors.nix
+++ home/hyprland/monitors.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       # NAME,RESOLUTION@REFRESHRATE,POSITON,SCALE
-      "DP-2,2560x1440@165,0x0,1"
+      "DP-3,2560x1440@165,0x0,1"
     ];
   };
 }
