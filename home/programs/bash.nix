3ca5aed1f479fa75f1daa43715bce3f5b76c883d
Nolan Locke <nolan.s.locke@gmail.com>
2024-02-13 22:07:12 -0700

    tty-clock alias

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -7,6 +7,7 @@
       nano = "nano -m";
       rebuild-nix = "sudo nixos-rebuild switch --flake ~/.config/nixos#desktop";
       cbonsai = "cbonsai --live --infinite";
+      tty-clock = "tty-clock -ctnB -C 5 -f \"%A, %b %d\"";
     };
   };
 }
