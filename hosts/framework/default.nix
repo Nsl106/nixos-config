a2433577f7e29e5608a9ff8204a6ad2ac1b96763
Nolan Locke <nolan.s.locke@gmail.com>
2024-04-26 10:00:41 -0700

    finish framework setup

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -11,15 +11,26 @@
   ];
 
   # Bootloader
-  #boot.loader.efi.canTouchEfiVariables = true;
-  #boot.loader.efi.efiSysMountPoint = "/boot";
-  #boot.loader.grub = {
-  #  devices = ["nodev"];
-  #  efiSupport = true;
-  #  enable = true;
-  #  # set $FS_UUID to the UUID of the EFI partition
-  #  useOSProber = true;
-  #};
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub = {
+    devices = ["nodev"];
+    efiSupport = true;
+    enable = true;
+    # set $FS_UUID to the UUID of the EFI partition
+    useOSProber = true;
+    
+    extraEntries = ''
+      menuentry "Windows" {
+        insmod part_gpt
+        insmod fat
+        insmod search_fs_uuid
+        insmod chain
+        search --fs-uuid --set=root 9E7D-7AA9
+        chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+      }
+    '';
+  };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   networking.hostName = "framework"; # Define your hostname.
